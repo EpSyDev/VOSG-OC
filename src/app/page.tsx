8d89caf8d6b943d01953799fe452aff70abe0dd5
EpSyDev <nicolas.smt@gmail.com>
2026-04-14 09:43:00 +0200

    tuile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -339,10 +339,6 @@ export default function Home() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               <GalleryItem category="Électricité" />
               <GalleryItem category="Climatisation" />
-              <GalleryItem category="Installation" />
-              <GalleryItem category="Maintenance" />
-              <GalleryItem category="Tertiaire" />
-              <GalleryItem category="Rénovation" />
             </div>
           </div>
         </section>
